76fdfb342423ff85f454a322329eb5c2183fc6c2
daxyman <danielfelipegg3005@gmail.com>
2024-10-24 23:09:38 -0400

    k

---
+++ CREATION DE SITES WEB/2024-10-24/notes.js
@@ -0,0 +1,26 @@
+//let x=5;
+//x= x+3;
+//alert(x)
+//const y = 5;
+//alert(y)
+//let bool = true;
+//console.log(typeof(bool));
+//console.log(typeof(x));
+//console.log(typeof(x));
+//console.log("le resultat est = " + y);
+//
+//let age;
+//age = prompt("doner votre age");
+//alert("ton age est "+ age);
+//
+
+var resultat = ((25-3) % 4) * (4 + 10) % 5;
+alert(resultat);
+
+// variable test --------
+let x =10;
+let y = ++x;
+
+console.log("x est égal à "+ x)
+console.log("y est égal à "+ y)
+
