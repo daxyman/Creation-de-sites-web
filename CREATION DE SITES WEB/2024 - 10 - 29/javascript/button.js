e2fdf3ae6950f03c3cc433d9d9204845819cd7a9
daxyman <danielfelipegg3005@gmail.com>
2024-10-29 13:27:43 -0400

    l

---
+++ CREATION DE SITES WEB/2024 - 10 - 29/javascript/button.js
@@ -0,0 +1,25 @@
+function showName(){
+    let name = prompt("Donner votre nom:");
+    alert("votre nom est " + name);
+}
+function date(){
+    let aujourdhui = new Date();
+    console.log(aujourdhui)
+}
+function example(){
+    let = a;
+    a = 5;
+}
+
+function birthCalculator(){
+let birthYear = prompt("annee naissance: ");
+
+// get the current year
+let currentYear = new Date().getFullYear();
+
+// calculate age
+let age = currentYear - birthYear;
+
+// print out age
+alert(`u are ${age} years old`);
+}
