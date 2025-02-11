9e385b2d54776c230ce1b7803e0fcb3a76a6cc86
daxyman <danielfelipegg3005@gmail.com>
2025-02-11 18:15:20 -0500

    dam

--- Personal html projects/responsive grid/script.js
+++
@@ -1,9 +0,0 @@
-function updateDimensions() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  document.getElementById("width").textContent = width;
-  document.getElementById("height").textContent = height;
-}
-
-window.addEventListener("resize", updateDimensions);
-document.addEventListener("DOMContentLoaded", updateDimensions);
